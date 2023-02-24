8ade9b81ab64f129c2dfdbfc53f390276ec894d1
Faez-B <faezbhatti@gmail.com>
2023-02-24 13:16:45 +0100

    Router links Home & About

--- assets/app.js
+++ assets/app.js
@@ -4,20 +4,23 @@ import ReactDOM from 'react-dom/client';
 import './styles/app.css';
 import './bootstrap';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
 import Home from './components/Home';
+import Layout from './components/Layout';
+import About from './components/About';
 
 function App() {
     return (
       <div className="App">
         <header>
-            <h1>React App</h1>
+            
         </header>
 
         <BrowserRouter>
             <Routes>
-                <Route path="/">
+                <Route path="/" element={<Layout/>}>
                     <Route index element={<Home />} />
+                    <Route path="about" element={<About />} />
                 </Route>
             </Routes>
         </BrowserRouter>
